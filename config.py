eaee126053d6ba73a821c1b9ebba79de22c68cb8
Kid-lumer <146942379+Kid-lumer@users.noreply.github.com>
2024-08-01 15:22:20 +0200

    test

---
+++ config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = '2120'
+JWT_SECRET_KEY = 'secret_key'
+MONGO_URI = "mongodb+srv://mxolisikidsibisi:<password>@cluster0.v3ik3z7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
